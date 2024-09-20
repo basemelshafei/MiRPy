02a854e5cc5a203697df2299316728e90e344d53
Basem Elshafei <60575846+basemelshafei@users.noreply.github.com>
2024-09-20 13:05:08 +0100

    Update kafka_consumer.py

--- Kafka4MiR/kafka_consumer.py
+++ Kafka4MiR/kafka_consumer.py
@@ -1,29 +1,27 @@
-# Kafka consumer to read the messages from the topic
 from kafka import KafkaConsumer
 import json
 import boto3
 import signal
 import sys
 
-# Initialize Kafka consumer
 consumer = KafkaConsumer(
     'my_mir_data',
     bootstrap_servers='localhost:9092',
     auto_offset_reset='earliest',
-    enable_auto_commit=True,  # Commits the read offset automatically
+    enable_auto_commit=True, 
     value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-    max_poll_records=1,  # Poll only one record at a time
-    fetch_max_wait_ms=100  # Set a short wait time for new records
+    max_poll_records=1,  
+    fetch_max_wait_ms=100  
 )
 
-# Initialize the S3 client
+
 s3_client = boto3.client('s3')
 
-# Define the S3 bucket name and folder
+
 bucket_name = 'mirdata'
 s3_folder = 'mir_data_from_kafka/'
 
-# Function to upload data to S3
+
 def upload_to_s3(data, file_name):
     try:
         json_data = json.dumps(data)
@@ -36,30 +34,27 @@ def upload_to_s3(data, file_name):
     except Exception as e:
         print(f"Error uploading to S3: {e}")
 
-# Signal handler for graceful shutdown
+
 def signal_handler(sig, frame):
     print('Shutting down consumer...')
     consumer.close()
     sys.exit(0)
 
-# Attach signal handler to interrupt signals
+
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
-# Consume messages and upload to S3
+
 def consume_data():
     for message in consumer:
         mir_data = message.value
         print(f"Received: {mir_data}")
 
-        # Make file name unique using partition, offset, and robot_name
         robot_name = mir_data.get('robot_name', 'UnknownRobot')
         s3_file_name = f"mir_data_{message.partition}_{message.offset}_{robot_name}.json"
 
-        # Upload to S3
         upload_to_s3(mir_data, s3_file_name)
 
-# Run the consumer
 if __name__ == "__main__":
     print("Starting Kafka consumer...")
     consume_data()
